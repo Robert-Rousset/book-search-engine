5611670cf048f10aa5cb57863f4dd030ec72d62d
Robert Rousset <whyisrob@hotmail.com>
2021-08-01 20:06:43 +0800

    added mutations and queries to the frontend

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -10,10 +10,26 @@ const resolvers = {
     getSingleUser: async (parent, { username }) => {
       return User.findOne({ username }).populate("savedBooks");
     },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate("thoughts");
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
   },
   Mutation: {
     createUser: async (parent, { body }) => {
       return User.create(body);
+    },
+    login: async (parent, { body }) => {
+      return User.findOne({ email: body.email }, { password: body.password });
+    },
+    saveBook: async (parent, { body }) => {
+      return User.findOneAndUpdate(
+        { _id: user._id },
+        { $addToSet: { savedBooks: body } },
+        { new: true, runValidators: true }
+      );
     },
   },
 };
